0bd8991c78803d2d10a8036149419b8a8931048a
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-05-01 18:15:46 -0700

    Changed name to Data Generator

--- orgInit.sh
+++ orgInit.sh
@@ -1,11 +1,11 @@
 # Create the org
-sfdx force:org:create -f config/project-scratch-def.json -a data-gen
+sfdx force:org:create -f config/project-scratch-def.json --setalias data-generator --setdefaultusername
 
 # Push the metadata into the new scratch org.
 sfdx force:source:push
 
 # Assign user the permset
-sfdx force:user:permset:assign -n DataGeneration
+sfdx force:user:permset:assign -n DataGenerator
 
 # Set the default password.
 sfdx shane:user:password:set -g User -l User -p salesforce1
